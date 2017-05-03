38d9f4a0a27cb3df2aa7dffff4b9cb858987c84a
Paige Goulding <paige.goulding11@gmail.com>
2017-05-03 17:30:31 -0400

    ahhhhh

--- classifier.py
+++ classifier.py
@@ -33,13 +33,13 @@ def extract_wrd_bigrams(sentences, positions):
     i = 0
     all_before_words = []  # list of words preceding it. I was going to make it a tuple, but the second part would always be "it" so that seemed silly
     all_after_words = []
-    while i < len(sentences):  # this while loop finds all the different words before and after eacn instance of "it"
+    while i < len(sentences):  # this while loop finds all the different words before and after each instance of "it"
         posn = int(positions[i])
         sent = nltk.word_tokenize(sentences[i])
         if posn > 1 and sent[(posn-2)] not in all_before_words:
             word_before = lemmatizer.lemmatize(sent[posn-2])
             all_before_words.append(word_before)
-        if posn < len(sent) and sent[(posn)] not in all_after_words:
+        if posn < len(sent) and sent[posn] not in all_after_words:
             word_after = lemmatizer.lemmatize(sent[posn])
             all_after_words.append(word_after)
         i += 1
@@ -63,9 +63,9 @@ def extract_POS_bigrams(sentences, positions):
 
 
 
-def get_feat_vect(all_before_words, all_after_words, sentences, positions):
+def get_feat_vect(all_before_words, all_after_words, all_before_POS, all_after_POS, sentences, positions):
     j = 0
-    wrd_array = np.zeros([len(sentences), (len(all_after_words)+len(all_before_words))])
+    wrd_array = np.zeros([len(sentences), (len(all_after_words)+len(all_before_words)+len(all_before_POS)+len(all_after_POS))])
     while j < len(sentences):  # this while loop finds all the different words before and after each instance of "it"
         posn = int(positions[j])
         sent = nltk.word_tokenize(sentences[j])
@@ -76,26 +76,40 @@ def get_feat_vect(all_before_words, all_after_words, sentences, positions):
             for word in all_after_words:
                 if posn < len(sent) - 1 and word == sent[posn]:
                     wrd_array[j][k] = 1
+            for pos in all_before_POS:
+                if posn > 1 and pos == sent[posn-2]:
+                    wrd_array[j][k] = 1
+            for pos in all_after_POS:
+                if posn < len(sent) - 1 and pos == sent[posn]:
+                    wrd_array[j][k] = 1
         j += 1
     return wrd_array
            
 answers, positions, sentences = read_in_ACLData(path) # self-explanatory
-before_words,after_words = extract_wrd_bigrams(sentences, positions) # get bag (bags) of words
+before_words, after_words = extract_wrd_bigrams(sentences, positions) # get bag (bags) of words
+
+print(before_words)
+print(after_words)
+
 before_POS, after_POS = extract_POS_bigrams(sentences, positions)
-feature_vector = get_feat_vect(before_words,after_words,sentences,positions) # use bag of words and sentences to get feature vectors
+
+print(before_POS)
+print(after_POS)
+
+feature_vector = get_feat_vect(before_words,after_words, before_POS, after_POS, sentences, positions) # use bag of words and sentences to get feature vectors
 # print(wrd_bg_ft[0])
 # print(len(answers)) #these are just little check-ins. change as needed
 # print(len(positions))
 # print(len(sentences))
 
 # extracting testing data
-testanswers,testpositions,testsentences = read_in_ACLData(testpath)
+testanswers, testpositions, testsentences = read_in_ACLData(testpath)
 # don't need to get Bag Of Words, we're using the training bag against the test sentences
-test_feature_vector = get_feat_vect(before_words, after_words, testsentences, testpositions)  # use training bag of words and test sentences to get feature vectors
+test_feature_vector = get_feat_vect(before_words, after_words, before_POS, after_POS, testsentences, testpositions)  # use training bag of words and test sentences to get feature vectors
 Z = np.array(testanswers)
 
 Y = np.array(answers)  # This np array is now ready to be used in the classifier. That's all we need to do to it
-clf = svm.SVC()
+clf = svm.SVC(kernel='poyl', class_weight='balanced')
 clf.fit(feature_vector, Y)
 print(clf.predict(test_feature_vector))
 
